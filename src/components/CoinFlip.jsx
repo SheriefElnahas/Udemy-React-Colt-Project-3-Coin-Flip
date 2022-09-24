e03379734f36d4a7938e86773b141b174518af11
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-24 12:46:29 +0200

    finish Coin Flip Component state

--- src/components/CoinFlip.jsx
+++ src/components/CoinFlip.jsx
@@ -8,20 +8,31 @@ const coinTail = 'https://tricksupply.com/wp-content/uploads/2019/10/s-l1600.jpg
 class CoinFlip extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {imgSrc : ''};
+        this.state = {imgSrc : '', flips: 0, heads : 0 , tails: 0};
         this.flipImage = this.flipImage.bind(this);
     }
 
     flipImage() {
-        this.setState({imgSrc : coinHead});
+        if(this.state.imgSrc === '' ) {
+            this.setState({imgSrc: coinHead, heads: 1})
+        }
+        this.setState(previousState => {
+             return {flips: previousState.flips + 1};
+         
+        })
         
         if(this.state.imgSrc === coinHead) {
+            this.setState(previousState => {
+                return {imgSrc: coinTail, tails: previousState.tails + 1};
+            })
+        } 
 
-            this.setState({imgSrc : coinTail})
-        } else {
-
-            this.setState({imgSrc: coinHead});
-        }
+        if(this.state.imgSrc === coinTail) {
+            this.setState(previousState => {
+                    return {imgSrc: coinHead, heads: previousState.heads + 1};
+            })
+         }
+   
     }
 
 
@@ -32,6 +43,8 @@ class CoinFlip extends React.Component {
             <div className="CoinFlip">
                     <Coin  imgSource={this.state.imgSrc} />
                     <button onClick={this.flipImage}> Flip Mee</button>
+                     <p>Out of {this.state.flips} flips, there have been {this.state.heads} heads and {this.state.tails} tails</p>
+             
             </div>
         )
     }
