959f626051495342667a7fe233558026dc2d26be
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-24 09:32:02 +0200

    Coin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Coin from './components/Coin';
 
 class App extends React.Component {
   render() {
-    return <h1>Hello</h1>
+    return (
+      <div>
+        < Coin />
+      </div>
+    )
   }
 }
 
---
+++ src/components/Coin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Coin extends React.Component {
+    render() {
+        return (
+            <div>
+                <img style={{width: '200px'}} src='https://upload.wikimedia.org/wikipedia/commons/c/cd/S_Half_Dollar_Obverse_2016.jpg' alt="" />
+            </div>
+        )
+    }
+}
+
+export default Coin;
